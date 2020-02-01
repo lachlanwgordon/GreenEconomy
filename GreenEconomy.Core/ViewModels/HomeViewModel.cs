26e455bf32df9225791c3026b21a4fbc107135d3
Lachlan Gordon <lachlanwgordon@gmail.com>
2020-02-02 09:49:11 +1100

    Updated Web UI

---
+++ GreenEconomy.Core/ViewModels/HomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GreenEconomy.Core.Services;
+using MvvmHelpers.Commands;
+
+namespace GreenEconomy.Core.ViewModels
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        public HomeViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+
+        public ICommand RegisterCommand => new AsyncCommand(Register);
+
+        public async Task Register()
+        {
+            await NavigationService.OpenPageAsync<BusinessDetailsViewModel>();
+        }
+    }
+}
