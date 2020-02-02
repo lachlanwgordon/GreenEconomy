9fe899cb8df564c183e4aaafb20c2591661a17be
Lachlan Gordon <lachlanwgordon@gmail.com>
2020-02-03 01:45:22 +1100

    Wiring up of Xamarin Essentials Blazor.
    This can also be used in Functions for any APIs that are pure .Net core and don't rely on JSInterop or Browser APIs

---
+++ GreenEconomy.Functions/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GreenEconomy.Core.Models;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using GreenEconomy.Core;
+using DryIoc;
+using GreenEconomy.Core.Services;
+using Xamarin.Essentials.Interfaces;
+
+[assembly: FunctionsStartup(typeof(GreenEconomy.Functions.Startup))]
+namespace GreenEconomy.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var business = new Business();
+            var ioc = new IOC();
+            ioc.Initialize();
+            ioc.Container.Register<IGeocoding, Xamarin.Essentials.Blazor.Geocoding>();
+
+        }
+    }
+}
---
+++ Xamarin.Essentials.Blazor/Geocoding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Xamarin.Essentials.Blazor
+{
+    public class Geocoding : Xamarin.Essentials.Interfaces.IGeocoding
+    {
+        public Geocoding()
+        {
+        }
+
+        public Task<IEnumerable<Location>> GetLocationsAsync(string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Placemark>> GetPlacemarksAsync(Location location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Placemark>> GetPlacemarksAsync(double latitude, double longitude)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
